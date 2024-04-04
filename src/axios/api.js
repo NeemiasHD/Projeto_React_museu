54092d78d73b712e077bfce67f9cc3b355b08bea
Neemias Claudio <118923093+NeemiasHD@users.noreply.github.com>
2024-04-04 00:10:54 -0300

    Update api.js

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const itemFetch = axios.create({
-  baseURL: "https://localhost:7107",
+  baseURL: "https://525f-2804-d59-b336-8300-3437-f73a-7b73-a6ac.ngrok-free.app",
   headers: {
     "Content-Type": "application/json",
   }, 
