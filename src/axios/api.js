e88a8cad633d31c7a9c08cae363fa2030cfe76db
Neemias Claudio <118923093+NeemiasHD@users.noreply.github.com>
2024-04-04 00:25:12 -0300

    Update api.js

--- src/axios/api.js
+++ src/axios/api.js
@@ -4,6 +4,7 @@ const itemFetch = axios.create({
   baseURL: "https://525f-2804-d59-b336-8300-3437-f73a-7b73-a6ac.ngrok-free.app",
   headers: {
     "Content-Type": "application/json",
+       'ngrok-skip-browser-warning': 'skip-browser-warning'
   }, 
 });
 export default itemFetch;
